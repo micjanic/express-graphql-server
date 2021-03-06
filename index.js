0427906bfe3d1ff8c2fcdd7d81443b9f5a04ac08
Michael <micjanic20@gmail.com>
2021-03-05 22:19:09 -0600

    setup

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+
+const { graphql, buildSchema } = require('graphql');
+ 
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+ 
+const root = { hello: () => 'Hello world!' };
+
+const app = express();
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    rootValue: root,
+    graphiql: true,
+  }),
+);
+
+app.listen(4000);
